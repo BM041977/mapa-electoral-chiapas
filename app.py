0d86f867fb0a7831f44ed271f4aaf27997eefa55
Baldemar Maza <baldemarmaza@gmail.com>
2026-03-13 15:00:36 -0600

    fix sqlite threads

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ DB_PATH = os.path.join(BASE_DIR, "usuarios.db")
 # CONEXIÓN A BASE DE DATOS
 # -----------------------------
 def conectar_db():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
 
