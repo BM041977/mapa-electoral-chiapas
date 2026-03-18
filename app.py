632832c05c082ef6d79d41c596d14dcfcbd661e5
Baldemar Maza <baldemarmaza@gmail.com>
2026-03-18 15:27:39 -0600

    test simple

--- app.py
+++ app.py
@@ -1,56 +1,14 @@
-from flask import Flask, render_template, request, redirect, session
-import os
+from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY", "clave_default")
 
-# 🔐 USUARIO Y PASSWORD (Render o local)
-USER = os.environ.get("APP_USER", "Baldemar")
-PASSWORD = os.environ.get("APP_PASSWORD", "Victoria@Ever")
+@app.route("/")
+def home():
+    return "OK FUNCIONANDO"
 
-# -----------------------------
-# LOGIN
-# -----------------------------
-@app.route("/", methods=["GET", "POST"])
-def login():
-
-    if request.method == "POST":
-        usuario = request.form.get("usuario", "").strip()
-        password = request.form.get("password", "").strip()
-
-        if usuario == USER and password == PASSWORD:
-            session.clear()
-            session["logged_in"] = True
-            return redirect("/mapa")
-
-        return render_template("login.html", error="Usuario o contraseña incorrectos")
-
-    return render_template("login.html")
-
-
-# -----------------------------
-# MAPA (PROTEGIDO)
-# -----------------------------
 @app.route("/mapa")
 def mapa():
+    return "MAPA OK"
 
-    if not session.get("logged_in"):
-        return redirect("/")
-
-    return render_template("mapa.html")
-
-
-# -----------------------------
-# LOGOUT
-# -----------------------------
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect("/")
-
-
-# -----------------------------
-# RUN LOCAL
-# -----------------------------
 if __name__ == "__main__":
     app.run(debug=True)
