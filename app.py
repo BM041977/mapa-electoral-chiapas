b0ab28b852a3f131273b2a132b0395f12fc94f70
Baldemar Maza <baldemarmaza@gmail.com>
2026-03-18 15:47:00 -0600

    test mapa texto

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "OK FUNCIONANDO"
-
 @app.route("/mapa")
 def mapa():
-    return "MAPA OK"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    if not session.get("logged_in"):
+        return redirect("/")
+    return "MAPA OK FUNCIONANDO"
